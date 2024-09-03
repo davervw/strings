4f999590c8dcf03dcf2a8f500330c0259cd64a60
Dave Van Wagner <git@davevw.com>
2024-09-02 17:27:23 -0700

    lowercase source, add copyright, description

--- strings.asm
+++ strings.asm
@@ -1,71 +1,102 @@
-*=$033C
-STRINGS:
-    LDA $2B
-    STA $FB
-    LDA $2C
-    STA $FC
-    LDY #$00
-    STY $FF
-PROGRAM_LOOP:
-    LDA ($FB),Y
-    INY
-    STA $FD
-    LDA ($FB),Y
-    STA $FE
-    ORA $FD
-    BEQ STRINGS_EXIT
-    LDY #$03
-LINE_LOOP:
-    INY
-FIND_STRING:
-    LDA ($FB),Y
-    BEQ NEXT_LINE
-    CMP #$22
-    BEQ FOUND_STRING
-    INY
-    BNE FIND_STRING
-FOUND_STRING
-    LDA $C7
-    EOR #$12
-    STA $C7
-    INY
-STRING_LOOP:
-    LDA #$01
-    STA $D4
-    STA $D8
-    LDA ($FB),Y
-    BEQ NEXT_LINE
-    CMP #$22
-    BEQ LINE_LOOP
-    JSR $FFD2
-    LDA $D3
-    BEQ CHECK_PAUSE
-    CMP #$28
-    BNE NOPAGE
-CHECK_PAUSE:
-    INC $FF
-    LDA $FF
-    CMP #$18
-    BNE NOPAGE
-    LDA #$00
-    STA $FF
-    TYA
-    PHA
-PAUSE_PAGE:
-    JSR $FFE4
-    BEQ PAUSE_PAGE
-    PLA
-    TAY
-NOPAGE:
-    INY
-    BNE STRING_LOOP
-NEXT_LINE:
-    LDA $FD
-    STA $FB
-    LDA $FE
-    STA $FC
-    LDY #$00
-    BEQ PROGRAM_LOOP
-STRINGS_EXIT:
-    LDA #$0D
-    JMP $FFD2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; strings for C64 - display string constants in BASIC program and/or dir listing
+;; Copyright (c) 2024 by David R. Van Wagner
+;; davevw.com
+;; MIT LICENSE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MIT License
+;;
+;; Copyright (c) 2024 David R. Van Wagner
+;; davevw.com
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+*=$033c
+strings:
+    lda $2b
+    sta $fb
+    lda $2c
+    sta $fc
+    ldy #$00
+    sty $ff
+program_loop:
+    lda ($fb),y
+    iny
+    sta $fd
+    lda ($fb),y
+    sta $fe
+    ora $fd
+    beq strings_exit
+    ldy #$03
+line_loop:
+    iny
+find_string:
+    lda ($fb),y
+    beq next_line
+    cmp #$22
+    beq found_string
+    iny
+    bne find_string
+found_string
+    lda $c7
+    eor #$12
+    sta $c7
+    iny
+string_loop:
+    lda #$01
+    sta $d4
+    sta $d8
+    lda ($fb),y
+    beq next_line
+    cmp #$22
+    beq line_loop
+    jsr $ffd2
+    lda $d3
+    beq check_pause
+    cmp #$28
+    bne nopage
+check_pause:
+    inc $ff
+    lda $ff
+    cmp #$18
+    bne nopage
+    lda #$00
+    sta $ff
+    tya
+    pha
+pause_page:
+    jsr $ffe4
+    beq pause_page
+    pla
+    tay
+nopage:
+    iny
+    bne string_loop
+next_line:
+    lda $fd
+    sta $fb
+    lda $fe
+    sta $fc
+    ldy #$00
+    beq program_loop
+strings_exit:
+    lda #$0d
+    jmp $ffd2
