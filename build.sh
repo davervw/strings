6906d73f9ccd2c73f81d8df5b4038cdc4070161c
Dave Van Wagner <git@davevw.com>
2024-09-02 17:56:41 -0700

    add full program listing from build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 export PROG=strings
 mkdir build 2>/dev/null
-${ACME}/acme -f cbm -o build/${PROG}.prg -l build/${PROG}.lbl ${PROG}.asm \
+${ACME}/acme -f cbm -o build/${PROG}.prg -l build/${PROG}.lbl -r build/${PROG}.lst ${PROG}.asm \
 && ${VICE}/c1541 ${PROG}.d64 -attach ${PROG}.d64 8 -delete ${PROG} -write build/${PROG}.prg ${PROG}\
 && ${VICE}/x64sc -moncommands build/${PROG}.lbl -autostart ${PROG}.d64 >/dev/null 2>&1 &
